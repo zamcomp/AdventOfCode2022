05194503f5f4c4814bf3614ce0f2172346b3a555
Chris Platt <chrisplatt@zamcomp.co.uk>
2022-12-06 19:13:06 +0000

    Day 6 - Powershell

---
+++ 6/6-2.ps1
@@ -0,0 +1,23 @@
+
+
+
+$file = "6\_Input\6.txt"
+$fileContent = Get-Content $file
+
+$messageLength = $fileContent.Length
+$currentIndex = 0
+$foundMarker = -1
+
+while($currentIndex -lt ($messageLength - 3) -and ($foundMarker -eq -1))
+{
+    $string = $fileContent.Substring($currentIndex,14)
+
+    if(-not($string.ToCharArray() | group -NoElement | ? Count -gt 1))
+    {
+        $foundMarker = $currentIndex + 14
+    }
+
+    $currentIndex++
+}
+
+Write-Host $foundMarker -ForegroundColor Blue
